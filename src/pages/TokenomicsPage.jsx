c9d2d928d54757ce386395305a4d6ab70cb98733
Bipuli <bipuli@Bipulis-MacBook-Air.local>
2026-06-15 10:01:50 +0530

    new motions add to the bands

--- src/pages/TokenomicsPage.jsx
+++ src/pages/TokenomicsPage.jsx
@@ -1,4 +1,7 @@
 import tokenomics from "../assets/tokenomics.png";
+import tokenomicsDetails from "../assets/tokenomics-details.png";
+import tokenBand1 from "../assets/token-band-1.png";
+import tokenBand2 from "../assets/token-band-2.png";
 
 function TokenomicsPage({ onBack }) {
   return (
@@ -13,9 +16,29 @@ function TokenomicsPage({ onBack }) {
         className="tokenomics-title-img"
       />
 
-      <section className="tokenomics-content">
-        {/* Add Tokenomics content here later */}
-      </section>
+      {/* Upper band on black background */}
+      <div className="token-band token-band-black">
+        <div className="token-band-track move-left">
+          <img src={tokenBand1} alt="" />
+          <img src={tokenBand1} alt="" />
+        </div>
+      </div>
+
+      <div className="tokenomics-details-wrap">
+        <img
+          src={tokenomicsDetails}
+          alt="Tokenomics Details"
+          className="tokenomics-details-img"
+        />
+
+        {/* Lower band on yellow details image */}
+        <div className="token-band token-band-bottom">
+          <div className="token-band-track move-right">
+            <img src={tokenBand2} alt="" />
+            <img src={tokenBand2} alt="" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
